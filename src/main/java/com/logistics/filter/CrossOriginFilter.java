d029e715f15459860f52fe3d5f7ba3bd36903e8c
lizhen <lizhn95@163.com>
2018-03-04 00:29:48 +0800

    init

---
+++ src/main/java/com/logistics/filter/CrossOriginFilter.java
@@ -0,0 +1,44 @@
+package com.logistics.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 支持跨域访问,方便前端调试
+ * 
+ * @author lizhen
+ */
+public class CrossOriginFilter implements Filter {
+
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Max-Age", "1728000");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, HEAD, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,YSession,authorization, ymmoa-passport, clientTime");
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        if ( request.getMethod().compareTo("OPTIONS") == 0 ) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(servletRequest, response);
+        }
+    }
+
+    public void destroy() {
+
+    }
+}
